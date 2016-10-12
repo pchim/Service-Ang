83353594546af5ba1e424c47f0acdcf9c522d972
Peter Chim <pchim24@gmail.com>
2016-10-12 15:42:04 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noteriousApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the noteriousApp
+ */
+angular.module('noteriousApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
